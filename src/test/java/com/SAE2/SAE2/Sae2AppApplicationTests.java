c1f7200d388328e9fd40c8f6ffc80ce0e600aa99
Ramilatif <latiframi18@gmail.com>
2024-05-01 18:49:02 +0200

    init Java

---
+++ src/test/java/com/SAE2/SAE2/Sae2AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SAE2.SAE2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sae2AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
