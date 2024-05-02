d41671a3d8a553fb9b1ef7911b9dc3cc3d1983f9
Ramilatif <latiframi18@gmail.com>
2024-05-03 00:01:42 +0200

    Revert "Reorganosation architecture of project & init API"
    This reverts commit 0041a967889b12e29795a730e279a6a9a221283c.

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Appbar from './Composants/Appbar';
+import PagePrincipale from './PageApplication/PagePrincipale'
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar/>
+      <PagePrincipale/>
+    </div>
+  );
+}
+
+export default App;
