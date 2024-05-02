3e90411b5a2b142509da9e4e2eb1a8b42238436b
Ramilatif <latiframi18@gmail.com>
2024-05-02 23:14:58 +0200

    Reorganisation of the project architecture and start of API creation

---
+++ src/main/java/com/SAES4/SAE2/Sae2Application.java
@@ -0,0 +1,13 @@
+package com.SAES4.SAE2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sae2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sae2Application.class, args);
+	}
+
+}
