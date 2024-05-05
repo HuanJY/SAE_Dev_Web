0ee490877ff89c551a1cc22475923134d6d8cad6
Nguyen Christian <ngchristian93@gmail.com>
2024-05-05 23:02:23 +0200

    modif css pour l'affichage

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import './App.css';
 import LoginPage from './components/LoginPage';
+
+// Import des barres
+import HeadBar from './components/HeadBar'
+import MainBar from './components/MainBar';
+import TabBar from './components/TabBar';
+
+// Import des différentes pages
+import MenuPrincipale from './PageApplication/MenuPrincipale';
+import Modele from './PageApplication/Modele';
 
 function App() {
   return (
     <div>
-      <LoginPage />
+      <HeadBar/>
+      <TabBar/>
+      <Modele/>
     </div>
   );
 }
