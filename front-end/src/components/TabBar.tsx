1e824eef28cdc7f1e8e84625f6318c8cb9ad4e7a
Nguyen Christian <ngchristian93@gmail.com>
2024-05-06 23:47:10 +0200

    supp élément / Ajout des Link pour les router

--- front-end/src/components/TabBar.tsx
+++ front-end/src/components/TabBar.tsx
@@ -4,21 +4,22 @@ import Button from '@mui/material/Button';
 import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard';
 import GroupsIcon from '@mui/icons-material/Groups';
 import SettingsIcon from '@mui/icons-material/Settings';
-import WorkspaceDialog from './WorkspaceDialog';
 import BurstModeIcon from '@mui/icons-material/BurstMode';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
 
 interface ButtonData {
     text: string;
     icon: JSX.Element;
+    path: string;
 }
 
 const buttonsData: ButtonData[] = [
-    { text: 'Menu Principale', icon: <MenuIcon /> },
-    { text: 'Tableau', icon: <SpaceDashboardIcon /> },
-    { text: 'Membres', icon: <GroupsIcon /> },
-    { text: 'Modèles', icon : <BurstModeIcon/>},
-    { text: 'Paramètres', icon: <SettingsIcon /> }
+    { text: 'Menu Principale', icon: <MenuIcon />, path: '/'},
+    { text: 'Tableau', icon: <SpaceDashboardIcon />, path: '/Liste'},
+    { text: 'Membres', icon: <GroupsIcon />, path: '/Membres' },
+    { text: 'Modèles', icon : <BurstModeIcon/>, path: 'Modele'},    
+    { text: 'Paramètre', icon: <SettingsIcon />, path: 'Parametre'}
 ];
 
 const ResponsiveBarreTableau: React.FC = () => {
@@ -34,49 +35,34 @@ const ResponsiveBarreTableau: React.FC = () => {
     const handleClose = (title: string) => {
         setOpen(false);
         if (title) {
-            setButtonsTab([...buttonsTab, { text: title }]);
+            setButtonsTab([...buttonsTab, { text: title, path: `/path/${title.toLowerCase().replace(/\s+/g, '-')}` }]);
         }
     };
 
     return (
-        <div className="page">
-            <div className="left-side">
+        <div className="left-side">
 
-                <Box>
-                    <p style={{ paddingTop: '5px' }}> Vous êtes ici </p>
-                    
-                    <Button sx={{ my: 2, color: 'black', fontFamily: 'monospace', display: 'block', textTransform: 'none', width:'100%', border:'solid black', borderRadius:'20px', height:'90px'}}>
-                        A remplir
-                    </Button>
-                </Box>
+            <p> Vous êtes ici </p>
 
-                <hr/>
+            <Box sx={{ my: 2, color: 'black', fontFamily: 'monospace', display: 'flex', textTransform: 'none', width:'100%', border:'solid black', 
+                borderRadius:'20px', height:'90px', justifyContent: 'center', alignItems: 'center'}}>
+                A remplir
+            </Box>
 
-                <Box sx={{ flexGrow: 1}}>
-                     {buttonsData.map((button, index) => (
-                    <Button key={index} sx={{ my: 2, fontSize:'100%', color: 'black', fontFamily: 'monospace', display: 'block', width: '100%', border: 'solid black', borderRadius: '20px', height: '90px'}}>
-                        <p>{button.icon} <br/> {button.text}</p>
-                    </Button>
+            <hr/>
+
+            <Box sx={{ flexGrow: 1}}>
+                 {buttonsData.map((button, index) => (
+                    <Link key={index} to={button.path} style={{ textDecoration: 'none'}}>
+                        <Button key={index} sx={{ my: 2, fontSize:'100%', color: 'black', fontFamily: 'monospace', display: 'block', 
+                            width: '100%', border: 'solid black', borderRadius: '20px', height: '90px'}}>
+                            <p>{button.icon} <br/> {button.text}</p>
+                        </Button>
+                    </Link>
                 ))}
-                </Box>
+            </Box>
 
-                <hr/>
-
-                <p style={{ paddingTop: '5px' }}> Vos tableaux</p>
-
-                <Box sx={{ flexGrow: 1 }}>
-                    {buttonsTab.map((button, index) => (
-                    <Button key={index} sx={{ my: 2, color: 'black', fontFamily: 'monospace', display: 'block', textTransform: 'none', width: '100%', border: 'solid black', borderRadius: '20px', height: '90px' }}>
-                        <p>{button.text}</p>
-                    </Button>
-                    ))}
-
-                    <Button sx={{ my: 2, color: 'black', fontFamily: 'monospace', display: 'block', textTransform: 'none', width: '100%', border: 'solid black', borderRadius: '20px', height: '90px' }} onClick={handleClickOpen}>
-                        Ajouter un tableau
-                    </Button>
-                    <WorkspaceDialog open={open} handleClose={handleClose} />
-                </Box>
-            </div>
+            <hr/>
         </div>
     )
 };
