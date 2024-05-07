4bb00e0a444ce81248600811090b4a13c7160d0a
Nguyen Christian <ngchristian93@gmail.com>
2024-05-07 02:29:26 +0200

    Ajout de changement de titre

--- front-end/src/PageApplication/ToDoList.tsx
+++ front-end/src/PageApplication/ToDoList.tsx
@@ -2,9 +2,22 @@ import React, { useState } from 'react';
 import { Box, Button, ListItem} from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import TuneIcon from '@mui/icons-material/Tune';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
 
 const ResponsiveTodoList: React.FC = () => {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const [lists, setLists] = useState<string[][]>([['']]);
+    const [editTitleOpen, setEditTitleOpen] = useState(false);
+    const [newTitle, setNewTitle] = useState("Nouvelle liste");
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+      };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     const handleAddList = () => {
         setLists([...lists, ['']]);
@@ -16,22 +29,49 @@ const ResponsiveTodoList: React.FC = () => {
         setLists(updatedLists);
     };
 
+    const handleEditTitle = () => {
+        setEditTitleOpen(true);
+        handleClose(); // Ferme également le menu lorsqu'on commence à modifier le titre
+    };
+
+    const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNewTitle(event.target.value);
+    };
+
+    const handleTitleSave = () => {
+        // Ici, vous pouvez ajouter la logique pour enregistrer le nouveau titre
+        console.log("Nouveau titre :", newTitle);
+        setEditTitleOpen(false);
+    };
+
     return (
         <div className="right-side" style={{ overflowX: 'auto'}}>
+
             <div style={{ display: 'flex', flexWrap: 'nowrap', width: 'max-content'}}>
+
                 {lists.map((tasks, listIndex) => (
                     <Box key={listIndex} sx={{ width: '400px', height:'', backgroundColor: 'white', fontFamily: 'monospace', overflow: 'hidden', border: 'solid black', borderRadius: '20px', marginRight: '20px'}}>
-                        <Box sx={{ backgroundColor: 'grey', display: 'flex', justifyContent: 'center', alignItems: 'center', borderBottom: 'solid black', padding:'2%' }}>
-                            <Box sx={{ width: '85%', textAlign: 'center', color:'white'}}>
-                                <p>Nouvelle liste</p>
+
+                        <Box sx={{ backgroundColor: 'grey', display: 'flex', justifyContent: 'center', alignItems: 'center', borderBottom: 'solid black', padding:'2%'}}>
+
+                            <Box  sx={{width: '85%', color: 'white', float:'left', textAlign:'center'}} onClick={handleEditTitle}>
+                                {editTitleOpen ? (<TextField size="small" autoFocus value={newTitle} onChange={handleTitleChange} onBlur={handleTitleSave}/>) : (
+                                    <p>{newTitle}</p>
+                                )}
                             </Box>
 
-                            <Button sx={{textAlign: 'center', border: 'solid black', borderRadius: '20px', color: 'white' }}>
+                            <Button sx={{border: 'solid black', borderRadius: '20px', color: 'white', float:'right'}} onClick={handleClick}>
                                 <TuneIcon/>
-                            </Button>
+                            </Button> 
+                            
+
+                            <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+                                <MenuItem onClick={handleClose}>Option 1</MenuItem>
+                                <MenuItem onClick={handleClose}>Archiver cette liste</MenuItem>
+                            </Menu>
                         </Box>
 
-                        <Box sx={{ maxHeight: '580px', overflowY: 'auto', padding:'1%',backgroundImage:'url(Modele_Liste/Colore.jpeg)', backgroundSize: 'cover'}}>
+                        <Box sx={{ maxHeight: '580px', overflowY: 'auto', padding:'1%',backgroundImage:'url(Modele_Liste_BG/Colore.jpeg)', backgroundSize: 'cover'}}>
                             {tasks.map((task, taskIndex) => (
                                 <ListItem key={taskIndex} >
                                     <Button sx={{ width: '100%', height:'auto', textAlign: 'center', border:'solid black', borderRadius:'20px', backgroundColor:'grey', color:'white', overflowX: 'auto'}}>
