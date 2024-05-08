bf74ead9447439eae3227fe624ff95345a646637
Ramilatif <latiframi18@gmail.com>
2024-05-08 23:45:31 +0200

    API to send data for Lists

---
+++ src/main/java/com/SAES4/SAE2/models/list/ListAzu.java
@@ -0,0 +1,21 @@
+package com.SAES4.SAE2.models.list;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "Lists")
+public class ListAzu {
+    @Id
+    @Column(name = "idList")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idList;
+    @Column(name = "listName")
+    private String boardName;
+    @Column(name = "idBoard")
+    private Integer idBoard;
+}
+
---
+++ src/main/java/com/SAES4/SAE2/repositories/ListRepositories.java
@@ -0,0 +1,12 @@
+package com.SAES4.SAE2.repositories;
+
+
+import com.SAES4.SAE2.models.list.ListAzu;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ListRepositories extends CrudRepository<ListAzu, Integer> {
+
+    List<ListAzu> findByIdBoard(Integer idBoard);
+}
