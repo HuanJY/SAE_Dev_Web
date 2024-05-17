ef0728126a4fa8feb64834b9e5a8979d5d508aeb
Bafy78 <30290088+Bafy78@users.noreply.github.com>
2024-05-17 19:46:47 +0200

    added missing config for logging-in

--- src/main/java/com/SAES4/SAE2/controllers/UserController.java
+++ src/main/java/com/SAES4/SAE2/controllers/UserController.java
@@ -5,6 +5,10 @@ import com.SAES4.SAE2.models.user.User;
 import com.SAES4.SAE2.repositories.UserRepository;
 import com.SAES4.SAE2.services.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
@@ -16,7 +20,10 @@ public class UserController {
 
     @Autowired
     private AuthenticationService authService;
-    
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody User newUser) {
         if (userRepository.existsByLoginName(newUser.getLoginName())) {
@@ -30,15 +37,17 @@ public class UserController {
         return ResponseEntity.ok("User registered successfully!");
     }
 
-
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
         User user = userRepository.findByLoginName(loginRequest.getLoginName());
         if (user != null && authService.checkPassword(loginRequest.getPassword(), user.getPasswordHash())) {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getLoginName(), loginRequest.getPassword())
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
             return ResponseEntity.ok("Login successful for user: " + user.getLoginName());
         } else {
             return ResponseEntity.badRequest().body("Login failed: Invalid username or password");
         }
     }
-
 }
