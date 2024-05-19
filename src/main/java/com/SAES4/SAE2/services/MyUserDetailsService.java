d1124124b0138227af8591a827d7440adaada34b
Bafy78 <30290088+Bafy78@users.noreply.github.com>
2024-05-19 18:35:44 +0200

    added missing config for logging-in (#16)

---
+++ src/main/java/com/SAES4/SAE2/services/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.SAES4.SAE2.services;
+
+import com.SAES4.SAE2.models.user.User;
+import com.SAES4.SAE2.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByLoginName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getLoginName(), user.getPasswordHash(), new ArrayList<>());
+    }
+}
