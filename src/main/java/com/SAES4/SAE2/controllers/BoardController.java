bde3d87802285d6ef118204f1c06123be03745c9
Ramilatif <latiframi18@gmail.com>
2024-05-26 21:22:14 +0200

    url API & DB script Rectified

--- src/main/java/com/SAES4/SAE2/controllers/BoardController.java
+++ src/main/java/com/SAES4/SAE2/controllers/BoardController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/board")
+@RequestMapping(path = "/board")
 public class BoardController {
 
     private final BoardService boardService;
--- src/main/java/com/SAES4/SAE2/controllers/ListController.java
+++ src/main/java/com/SAES4/SAE2/controllers/ListController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/list")
+@RequestMapping(path = "/list")
 public class ListController {
 
     private final ListService listService;
