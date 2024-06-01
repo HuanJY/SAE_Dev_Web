6d8f7bd348d5a501d951d86917ae7aec59faba38
Nguyen Christian <ngchristian93@gmail.com>
2024-06-01 02:52:03 +0200

    ajout même requete que le fichier board

--- front-end/src/components/MainBar.tsx
+++ front-end/src/components/MainBar.tsx
@@ -1,70 +1,122 @@
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
+import React, { useEffect, useState } from 'react';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard';
 import SettingsIcon from '@mui/icons-material/Settings';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import SetTitleBoard from '../BoardPage/SetTitleBoard';
+import axios from 'axios';
 
 interface ButtonData {
+    text: string;
+    path: string;
+}
+
+interface ButtonNav {
     text: string;
     icon: JSX.Element;
     path: string;
 }
 
-interface ButtonTab {
-    text: string;
-    path: string;
-}
-
-const buttonsData: ButtonData[] = [
-    { text: 'Tableau', icon: <SpaceDashboardIcon/>, path:'/'},
-    { text: 'Paramètre', icon: <SettingsIcon/>, path:'/Parametre' }
-];
-
 const ResponsiveMainBar: React.FC = () => {
+
+    const buttonsNav: ButtonNav[] = [
+        { text: 'Tableau', icon: <SpaceDashboardIcon />, path: '/accueil'},
+        { text: 'Paramètre', icon: <SettingsIcon />, path: '/Parametre'}
+    ];
+
     const [open, setOpen] = useState<boolean>(false);
-    const [buttonsTab, setButtonsTab] = useState<ButtonTab[]>([
-        { text: 'Exemple tableau', path: '/Liste'}
-    ]);
-    
+    const [error, setError] = useState<string>('');
+    const [buttonsTab, setButtonsTab] = useState<ButtonData[]>([]);
+
+    useEffect(() => {
+        const fetchBoards = async () => {
+            try {
+                const response = await axios.post(`api/board/user`, {
+                }, {
+                    withCredentials: true // Important pour inclure les cookies de session
+                });
+                const boards = response.data.map((board: any) => ({
+                    text: board.boardName,
+                    path: `/board/${board.idBoard}`
+                }));
+                setButtonsTab(boards);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des tableaux :', error);
+            }
+        };
+
+        fetchBoards();
+    }, []);
+
     const handleClickOpen = () => {
-      setOpen(true);
+        setOpen(true);
     };
-  
-    const handleClose = (title: string) => {
-        setOpen(false);
-        if (title) {
-            setButtonsTab([...buttonsTab, { text: title, path:`/path/${title.toLowerCase().replace(/\s+/g, '-')}`}]);
+
+    const handleClose = async (title: string) => {
+        if (!title.trim()) {
+            setError('Veuillez saisir un titre valide pour le tableau.');
+            return;
         }
+
+        try {
+            const response = await axios.post('api/board/addBoard', {
+                boardName: title
+            }, {
+                withCredentials: true // Important pour inclure les cookies de session
+            });
+
+            console.log('Création du tableau réussi');
+
+            // Ajouter le nouveau tableau à l'état
+            const newButton: ButtonData = { text: title, path: `/board/${response.data.idBoard}` };
+            setButtonsTab([...buttonsTab, newButton]);
+            setError('');
+            setOpen(false);
+        } catch (error) {
+            console.error('Erreur lors de l\'ajout du tableau :', error);
+        }
+    };
+
+    const handleCancel = () => {
+        setOpen(false);
+        setError('');
     };
 
     return (
         <div className="left-side">
-
             <div>
-                {buttonsData.map((button, index) => (
-                <Link key={index} to={button.path} style={{textDecoration:'none'}}>
-                    <button type='button' className='buttonBar buttonForm' key={index}>
-                        <p>{button.icon} <br/> {button.text}</p>
+                <h3>Vue d'ensemble</h3>
+
+                <div>
+                    {buttonsNav.map((button, index) => (
+                        <Link key={index} to={button.path} style={{textDecoration: 'none'}}>
+                            <button type='button' className='buttonBar buttonForm' key={index}>
+                                <p>{button.icon} <br/> {button.text}</p>
+                            </button>
+                        </Link>
+                    ))}
+                </div>
+
+                <hr/>
+
+                <p style={{paddingTop: '5px'}}> Vos tableaux </p>
+
+                <div style={{flexWrap: 'wrap'}}>
+                    {buttonsTab.map((button, index) => (
+                        <Link key={index} to={button.path}>
+                            <button type='button' className='buttonBar buttonForm'>
+                                <p>{button.text}</p>
+                            </button>
+                        </Link>
+                    ))}
+                    <button type='button' className='buttonBar buttonForm' onClick={handleClickOpen}>
+                        <AddBoxIcon/> <p style={{marginLeft: '1%'}}>Ajouter un tableau</p>
                     </button>
-                </Link>
-            ))}
+                    <SetTitleBoard open={open} handleClose={handleClose} handleCancel={handleCancel} error={error}/>
+                </div>
             </div>
-
-            <hr/>
-
-            <p style={{paddingTop: '5px'}}> Vos tableaux </p>
-
-            <Box sx={{ flexGrow: 1 }}>
-                {buttonsTab.map((button, index) => (
-                    <Link key={index} to={button.path} style={{textDecoration:'none'}}>
-                        <button type='button' className='buttonBar buttonForm' key={index}>
-                            <p>{button.text}</p>
-                        </button>
-                    </Link>
-                ))}
-            </Box>
         </div>
-    )
+    );
 };
 
 export default ResponsiveMainBar;
